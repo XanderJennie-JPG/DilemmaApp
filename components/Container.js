4ef99efdc5f2ddcce24b678be30c623411c3fb3f
NabilNaou <nabilnaou@hotmail.com>
2022-11-22 21:44:01 +0100

    Added images and buttonstyling pt.1

--- components/Container.js
+++ components/Container.js
@@ -19,7 +19,7 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    marginHorizontal: 20,
+    marginHorizontal: 30,
     marginVertical: 10,
   },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,36 +1,76 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ImageBackground,
+  Image,
+} from "react-native";
 import React from "react-native";
 import Container from "../components/Container";
 import Datum from "../components/Datum";
 import GlobalStyle from "../components/GlobalStyle";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const user = "Placeholder";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <Container>
-      <View>
-        <Text style={[GlobalStyle.CustomFont, styles.Header]}>
-          Goedemiddag, {"\n"}
-          {user}
-        </Text>
-        <Text style={[GlobalStyle.CustomFont]}>
-          Het is vandaag{"\n"}
+      <Text style={[GlobalStyle.CustomFont, styles.Header, styles.TextBetween]}>
+        Goedemiddag, {"\n"}
+        {user}
+      </Text>
+      <Text style={[GlobalStyle.CustomFont, styles.TextBetween]}>
+        Het is vandaag{"\n"}
+        <Datum />
+      </Text>
+      <Text style={[GlobalStyle.CustomFont]}>
+        Wij hebben het dilemma spel {"\n"}geupdate op{" "}
+        <Text style={{ fontWeight: "bold" }}>
           <Datum />
         </Text>
-        <Text style={[GlobalStyle.CustomFont]}>
-          Wij hebben het dilemma spel {"\n"}geupdate op{" "}
-          <Text style={{ fontWeight: "bold" }}>
-            <Datum />
-          </Text>
-        </Text>
+      </Text>
+
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 75,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          resizeMode="contain"
+          source={require("../assets/Medtap.png")}
+        ></Image>
+      </View>
+      <View
+        style={{
+          position: "absolute",
+          top: 275,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <TouchableOpacity
-          style={styles.Button}
+          style={[styles.Button, styles.shadow, { flexDirection: "row" }]}
           onPress={() => navigation.navigate("Uitleg")}
         >
-          <Text style={[styles.ButtonText, GlobalStyle.CustomFont]}>
+          <Text style={[styles.ButtonText, GlobalStyle.CustomFontBold]}>
             Begin het dilemma spel
           </Text>
+          <Ionicons
+            name="arrow-forward-sharp"
+            size="20"
+            style={{ paddingLeft: 3 }}
+          ></Ionicons>
         </TouchableOpacity>
       </View>
     </Container>
@@ -40,19 +80,34 @@ const HomeScreen = ({ navigation }) => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  TextBetween: {
+    marginBottom: 18,
+  },
+  SquareBox: {
+    backgroundColor: "rgba(255, 255, 255, 0.15)",
+    borderRadius: 20,
+    border: 1,
   },
   Button: {
     backgroundColor: "#ffffff",
+    paddingVertical: 25,
+    paddingHorizontal: 15,
     borderRadius: 20,
   },
   Header: {
     fontSize: 30,
   },
   ButtonText: {
-    fontSize: 20,
+    fontSize: 15,
+  },
+  shadow: {
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 2.5,
+    elevation: 4,
   },
 });
