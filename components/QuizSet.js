2d5d42d104b8b6bfcb4a6644c93a942f8d7d554a
NabilNaou <nabilnaou@hotmail.com>
2022-11-29 16:24:12 +0100

    Start made on dilemmascreen

---
+++ components/QuizSet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function QuizSet() {
+  return (
+    <div>QuizSet</div>
+  )
+}
+
+export default QuizSet
--- screens/DilemmasScreen.js
+++ screens/DilemmasScreen.js
@@ -1,19 +1,37 @@
-import { Image, Text } from "react-native";
+import { Image, Text, StyleSheet, View } from "react-native";
 import React from "react-native";
+import { useState } from "react";
 import Container from "../components/Container";
-import TopLogo from "../components/TopLogo";
 import { TouchableOpacity } from "react-native";
+import GlobalStyle from "../components/GlobalStyle";
 
 const DilemmasScreen = ({ navigation: { goBack } }) => {
+  const [count, setCount] = useState(1);
   return (
     <Container>
-      <TopLogo />
-      <TouchableOpacity onPress={() => goBack()}>
-        <Image source={require("../assets/Back-arrow.png")} />
-      </TouchableOpacity>
-      <Text>Hi</Text>
+      <View style={styles.rowone}>
+        <TouchableOpacity onPress={() => goBack()}>
+          <Image source={require("../assets/Back-arrow.png")} />
+        </TouchableOpacity>
+        <Text style={[GlobalStyle.CustomFont, { alignSelf: "center" }]}>
+          Dilemma{" "}
+          <Text style={[GlobalStyle.CustomFontBold, { fontSize: 30 }]}>
+            {count}/10
+          </Text>
+        </Text>
+      </View>
     </Container>
   );
 };
 
 export default DilemmasScreen;
+
+const styles = StyleSheet.create({
+  rowone: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  dilemmatext: {
+    fontSize: 30,
+  },
+});
