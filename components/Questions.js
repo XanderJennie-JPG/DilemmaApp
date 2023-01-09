43416227ca3719b49c33f2cb64573c3fcaa9676c
NabilNaou <nabilnaou@hotmail.com>
2023-01-09 22:34:09 +0100

    Seperated questions from screen

---
+++ components/Questions.js
@@ -0,0 +1,49 @@
+const questions = [
+  {
+    text: "Je zit aan een balie en vindt daar een afdelingsbezettingsoverzicht (abo). Je staat onder druk om nog snel een rapportage op te stellen. Wat doe je?",
+    answers: [
+      {
+        text: "Je pakt het abo en gooit het weg in de daarvoor papierbak voor vertrouwelijke informatie. Je spreekt je collega’s niet aan want je vindt dat het ieders eigen verantwoordelijkheid is om de regels na te leven.",
+        value: "A",
+      },
+      {
+        text: "Je laat het abo liggen. Een collega heeft dit vast nodig.",
+        value: "B",
+      },
+      { text: "Je pakt het abo op en spreekt je collega’s aan.", value: "C" },
+    ],
+  },
+  {
+    text: "Een collega is nogal chaotisch en vergeet snel inloggegevens. Als je langs de werkplek van de collega loopt zie je een post-it briefje op het bureau geplakt met daarop de inloggegevens genoteerd. Vorige week heb je ook al een vervelende woordenwisseling gehad  met deze collega. Wat doe je?",
+    answers: [
+      { text: "Je spreekt je collega aan..", value: "A" },
+      {
+        text: "Je laat het er maar bij voor deze keer,  je ziet dat je collega het druk heeft en herinnert je nog de discussie van vorige week.",
+        value: "B",
+      },
+      {
+        text: "Er komen toch nooit onbevoegden op deze werkplek, laat maar zitten.",
+        value: "C",
+      },
+    ],
+  },
+  {
+    text: "Je werkzaamheden lopen uit en er zit al 15 minuten een patiënt op je te wachten. Je handelt snel en print de verpleegkundige overdracht uit. Bij de printer blijkt dat je per ongeluk de overdracht 2 keer hebt uitgeprint.   ",
+    answers: [
+      {
+        text: "Je gooit de extra kopie in een papierversnipperaar of daarvoor bestemde afgesloten bak aan het andere eind van de gang.",
+        value: "A",
+      },
+      {
+        text: "Je neemt beide documenten mee en  neemt je voor deze later in de papierversnipperaar te gooien.",
+        value: "B",
+      },
+      {
+        text: "Je gooit de extra kopie in de gewone prullenbak naast de printer. ",
+        value: "C",
+      },
+    ],
+  },
+];
+
+export default questions;
