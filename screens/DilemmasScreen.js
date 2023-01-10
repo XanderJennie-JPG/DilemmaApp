ea14fbd404be7f62c6a749622e8840b273cc967e
XanderJennie-JPG <xanderjennie21@gmail.com>
2023-01-11 00:45:32 +0100

    added styling for dilemma screen

--- screens/DilemmasScreen.js
+++ screens/DilemmasScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Image, Text, TouchableOpacity, StyleSheet, ImageBackground } from "react-native";
 import Container from "../components/Container";
 import GlobalStyle from "../components/GlobalStyle";
 import axios from "axios";
@@ -74,6 +74,34 @@ const DilemmasScreen = ({ navigation: { goBack } }) => {
 
   return (
     <Container>
+      <ImageBackground
+        resizeMode="cover"
+        style={[
+          styles.backgroundTop,
+          {
+            transform: [{ rotate: "-15deg" }],
+            height: 250,
+            width: 450,
+            top: -20,
+            left: -50,
+          },
+        ]}
+        source={require("../assets/background-top.png")}
+      />
+      <ImageBackground
+        resizeMode="cover"
+        style={[
+          styles.backgroundBottom,
+          {
+            transform: [{ rotate: "15deg" }],
+            height: 250,
+            width: 450,
+            top: 475,
+            alignSelf: "center",
+          },
+        ]}
+        source={require("../assets/background-top.png")}
+      />
       <View style={styles.rowone}>
         <TouchableOpacity onPress={() => goBack()}>
           <Image source={require("../assets/Back-arrow.png")} />
@@ -85,16 +113,24 @@ const DilemmasScreen = ({ navigation: { goBack } }) => {
           </Text>
         </Text>
       </View>
-      <Text>{questions[currentQuestion - 1].text}</Text>
+      <View style={[styles.box, styles.shadow]} blurRadius={8.5}>
+      <View style={styles.styleA}>
+      <Text style={[styles.ButtonText, styles.question, {marginTop: 25}]}>{questions[currentQuestion - 1].text}</Text>
       {questions[currentQuestion - 1].answers.map((answer, index) => (
         <View style={{ flexDirection: "row" }} key={answer.value}>
           {/*A, B and C. fromCharCode converts unicode to characters.*/}
-          <Text>{String.fromCharCode(65 + index)}:</Text>
-          <TouchableOpacity onPress={() => handleAnswer(answer.value)}>
-            <Text> {answer.text}</Text>
+          <View style={{flex: 1}}>
+          <Text style={[styles.ButtonText, {alignSelf: "center", top: 50, fontWeight: "bold", fontSize: 23}]}>{String.fromCharCode(65 + index)}:</Text>
+          </View>
+          <TouchableOpacity 
+          style={[styles.Button, styles.shadow]}
+          onPress={() => handleAnswer(answer.value)}>
+            <Text style={[styles.ButtonText, { left: 5 }]}> {answer.text}</Text>
           </TouchableOpacity>
         </View>
       ))}
+      </View>
+      </View>
       <View style={styles.rowtwo}>
         <TouchableOpacity
           onPress={handlePrevious}
@@ -126,6 +162,15 @@ const DilemmasScreen = ({ navigation: { goBack } }) => {
 export default DilemmasScreen;
 
 const styles = StyleSheet.create({
+  backgroundTop: {
+    position: "absolute",
+    top: -10,
+    left: 10,
+    right: 0,
+  },
+  backgroundBottom: {
+    position: "absolute",
+  },
   rowone: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -139,8 +184,8 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
   bgmiddle: {
-    backgroundColor: "rgba(255,255,255,0.15)",
-    height: 575,
+    backgroundColor: "rgba(255,255,255,0.4)",
+    height: 400,
     borderWidth: 1,
     borderColor: "rgba(255,255,255,0.18)",
     borderRadius: 20,
@@ -184,7 +229,7 @@ const styles = StyleSheet.create({
   },
   box: {
     opacity: 1,
-    backgroundColor: "#cceff9",
+    backgroundColor: "#cceff9", //change to "rgba(255,255,255,0.8)" for android?
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     borderBottomLeftRadius: 20,
