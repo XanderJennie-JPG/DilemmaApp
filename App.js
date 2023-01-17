e51f6691b919002499c692d6026bccbae8f0d159
NabilNaou <nabilnaou@hotmail.com>
2023-01-17 17:03:03 +0100

    Complete back-end finished

--- App.js
+++ App.js
@@ -7,44 +7,44 @@ import { StatusBar } from "expo-status-bar";
 import React from "react-native";
 import { useEffect, useState } from "react";
 import {
-  useFonts,
-  Montserrat_400Regular,
-  Montserrat_700Bold,
+	useFonts,
+	Montserrat_400Regular,
+	Montserrat_700Bold,
 } from "@expo-google-fonts/montserrat";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigatorScreen from "./navigation/AppNavigator";
 const App = () => {
-  let [fontsLoaded] = useFonts({
-    Montserrat: Montserrat_400Regular,
-    MontserratBold: Montserrat_700Bold,
-  });
+	let [fontsLoaded] = useFonts({
+		Montserrat: Montserrat_400Regular,
+		MontserratBold: Montserrat_700Bold,
+	});
 
-  const [hasSeenWelcome, setHasSeenWelcome] = useState(false);
+	const [hasSeenWelcome, setHasSeenWelcome] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        AsyncStorage.clear();
-        const value = await AsyncStorage.getItem("hasSeenWelcome");
-        if (value !== null) {
-          setHasSeenWelcome(true);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			try {
+				//AsyncStorage.clear();
+				const value = await AsyncStorage.getItem("hasSeenWelcome");
+				if (value !== null) {
+					setHasSeenWelcome(true);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		})();
+	}, []);
 
-  if (!fontsLoaded) {
-    return null;
-  }
+	if (!fontsLoaded) {
+		return null;
+	}
 
-  return (
-    <NavigationContainer>
-      <AppNavigatorScreen hasSeenWelcome={hasSeenWelcome} />
-      <StatusBar style="dark" />
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<AppNavigatorScreen hasSeenWelcome={hasSeenWelcome} />
+			<StatusBar style="dark" />
+		</NavigationContainer>
+	);
 };
 
 export default App;
